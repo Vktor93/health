3af7ea955cbe61850caa426c34b25f0f61fa11f5
viktor <viktoraju@gmail.com>
2018-02-02 03:42:48 -0600

    UPDATED, Splash.java - updating by adding animation to splash screen

--- app/src/main/java/com/vktor/healthy/Splash.java
+++ app/src/main/java/com/vktor/healthy/Splash.java
@@ -1,13 +1,29 @@
 package com.vktor.healthy;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Splash extends AppCompatActivity {
+    private TextView tv;
+    private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        tv = findViewById(R.id.txtLogo);
+        iv = findViewById(R.id.imgLogo);
+
+        Animation animDown = AnimationUtils.loadAnimation( this, R.anim.uptodown);
+        Animation animUp = AnimationUtils.loadAnimation( this,R.anim.downtoup);
+
+        iv.startAnimation(animDown);
+        tv.startAnimation(animUp);
+
     }
 }

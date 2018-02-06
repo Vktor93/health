5e81a4adc4076a5b53fab722ebfcd49e72f2c4f6
viktor <viktoraju@gmail.com>
2018-02-05 19:46:16 -0600

    UPDATED, Splash.java - apply butterknife

--- app/src/main/java/com/vktor/healthy/Splash.java
+++ app/src/main/java/com/vktor/healthy/Splash.java
@@ -7,23 +7,26 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class Splash extends AppCompatActivity {
-    private TextView tv;
-    private ImageView iv;
+    @BindView(R.id.txtLogo)
+    TextView logo;
+
+    @BindView(R.id.imgLogo)
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        tv = findViewById(R.id.txtLogo);
-        iv = findViewById(R.id.imgLogo);
+        ButterKnife.bind(this);
 
         Animation animDown = AnimationUtils.loadAnimation( this, R.anim.uptodown);
         Animation animUp = AnimationUtils.loadAnimation( this,R.anim.downtoup);
 
-        iv.startAnimation(animDown);
-        tv.startAnimation(animUp);
-
+        image.startAnimation(animDown);
+        logo.startAnimation(animUp);
     }
 }
